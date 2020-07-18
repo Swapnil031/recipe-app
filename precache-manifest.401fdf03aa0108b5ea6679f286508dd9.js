e7506cb30525ed2b672f3e51e42bb150785255df
Swapnil031 <swapnilaudichya@gmail.com>
2020-07-18 23:50:08 +0530

    Updates

---
+++ precache-manifest.401fdf03aa0108b5ea6679f286508dd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777efa30abe83a3078b4973dabb3f263",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "fecd5186cc59111fb4a1",
+    "url": "/recipe-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5440aba689d244b3df3d",
+    "url": "/recipe-app/static/css/main.5ca645e0.chunk.css"
+  },
+  {
+    "revision": "fecd5186cc59111fb4a1",
+    "url": "/recipe-app/static/js/2.751ababb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-app/static/js/2.751ababb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5440aba689d244b3df3d",
+    "url": "/recipe-app/static/js/main.78d3580e.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
