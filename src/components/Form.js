eced46fb81b4e414f3037559d422b190583884d6
Swapnil031 <swapnilaudichya@gmail.com>
2020-07-20 18:28:26 +0530

    Updated Files

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Form = props => (
   <form onSubmit={props.getRecipe} style={{ marginBottom: '2rem'}}>
-	<input className="form__input" type='text' name='recipeName' placeholder="Search for recipes"/>
+	<input className="form__input" type='text' name='recipeName' placeholder="Search for keywords"/>
 	<button className="form__button">SEARCH</button>
   </form>
 );
